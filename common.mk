211a2f79a03c2883c79c230990ec31bb6ab21316
sbrissen <sbrissen@hotmail.com>
2013-06-04 09:16:45 -0400

    Add gps conf files
    Change-Id: I0d6feb85ffa16832c613e396ab7fab434ea9e2a1

--- common.mk
+++ common.mk
@@ -19,3 +19,13 @@ COMMON_PATH := device/samsung/smdk4412-qcom-common
 # GPS
 PRODUCT_PACKAGES += \
     gps.default
+
+ifneq ($(filter t0ltecdma i605 l900 r950,$(TARGET_DEVICE)),)
+    GPS_CONF := $(COMMON_PATH)/gps/gps-cdma.conf
+else
+    GPS_CONF := $(COMMON_PATH)/gps/gps.conf
+endif
+
+PRODUCT_COPY_FILES += \
+    $(GPS_CONF):/system/etc/gps.conf
+
