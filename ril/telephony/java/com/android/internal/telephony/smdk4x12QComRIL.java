d62989f8f13fd9f5abcfff54228d3078ed27ec91
Michael Gernoth <michael@gernoth.net>
2015-12-16 12:01:43 -0500

    ril: Return static radio capability from getRadioCapability
    Change-Id: I6f4f003d3b050e955af5abf2cce10b577902a363

--- ril/telephony/java/com/android/internal/telephony/smdk4x12QComRIL.java
+++ ril/telephony/java/com/android/internal/telephony/smdk4x12QComRIL.java
@@ -653,4 +653,14 @@ public class smdk4x12QComRIL extends RIL implements CommandsInterface {
             result.sendToTarget();
         }
     }
+
+    @Override
+    public void getRadioCapability(Message response) {
+        riljLog("getRadioCapability: returning static radio capability");
+        if (response != null) {
+            Object ret = makeStaticRadioCapability();
+            AsyncResult.forMessage(response, ret, null);
+            response.sendToTarget();
+        }
+    }
 }
